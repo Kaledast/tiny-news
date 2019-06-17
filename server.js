526cc7d907bc632d662b66b7402e55b40b260bd8
Kaledast <stadel_katja@web.de>
2019-06-17 10:44:15 +0200

    initial commit

--- server.js
+++
@@ -1,9 +0,0 @@
-const port = process.env.PORT || 4000;
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.listen(port);
